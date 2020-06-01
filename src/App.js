de322af8099dbc48199b8acf9418977db7576fe0
curtismn87 <curtismn87@yahoo.com>
2020-05-31 22:28:06 -0400

    Can't iterate this json

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from "react-dom";
 import './App.css';
 
-const apiUrl = "https://api.github.com/users";
+const apiUrl = "http://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&api_key=***REMOVED***";
 
 function App() {
   const [items, setItems] = React.useState([]);
@@ -15,16 +15,41 @@ function App() {
       //console.log(data);
       setItems(data);
       console.log(data);
+
+// This function successfully iterates through the object, just need to do it cleaner
+function nestedLoop(obj) {
+    const res = {};
+    function recurse(obj, current) {
+        for (const key in obj) {
+            let value = obj[key];
+            if(value !== undefined) {
+                if (value && typeof value === 'object') {
+                    recurse(value, key);
+                    console.log("This is key: " + key)
+                } else {
+                    console.log("This is value: " + value)
+                    res[key] = value;
+                }
+            }
+        }
+    }
+    recurse(obj);
+    return res;
+};
+nestedLoop(data);
+
+
     }
     fetchData();
   }, []);
 
   return (
     <div class="container">
-      {items.map(item => (
-        <div class="card">
-          <img src={item.avatar_url} />
-          <div class="card-body">{item.login}</div>
+    // Need to iterate through here somehow
+    // Most likely use above function and add these divs instead of console.log
+      {Object.keys(items).map(item => (
+        <div class="card" key={item}>
+          <div class="card-body">This is item {item}</div>
         </div>
       ))}
       ;
