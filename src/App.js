f0f3f1585f121d4c9c7480ee021f51df3eb26cb8
staticsound <johngreedjr@gmail.com>
2020-06-01 21:40:31 -0400

    fix rendering of usda call iterate object - still needs style

--- src/App.js
+++ src/App.js
@@ -12,46 +12,21 @@ function App() {
       var data = await fetch(apiUrl).then(res => {
         return res.json();
       });
-      //console.log(data);
-      setItems(data);
-      console.log(data);
 
-// This function successfully iterates through the object, just need to do it cleaner
-function nestedLoop(obj) {
-    const res = {};
-    function recurse(obj, current) {
-        for (const key in obj) {
-            let value = obj[key];
-            if(value !== undefined) {
-                if (value && typeof value === 'object') {
-                    recurse(value, key);
-                    console.log("This is key: " + key)
-                } else {
-                    console.log("This is value: " + value)
-                    res[key] = value;
-                }
-            }
-        }
-    }
-    recurse(obj);
-    return res;
-};
-nestedLoop(data);
-
-
+      setItems(data.list.item);
     }
     fetchData();
   }, []);
 
+  const usdaItems = items.map(item => (
+      <div class="card" key={item.id}>
+        <div class="card-body">{item.name}</div>
+      </div>
+  ))
+
   return (
     <div class="container">
-    // Need to iterate through here somehow
-    // Most likely use above function and add these divs instead of console.log
-      {Object.keys(items).map(item => (
-        <div class="card" key={item}>
-          <div class="card-body">This is item {item}</div>
-        </div>
-      ))}
+      {usdaItems}
       ;
     </div>
   );
