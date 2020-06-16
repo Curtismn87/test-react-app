c3aedf3d3efb6dcc6f44f8ca97d34626f8aa369a
staticsound <johngreedjr@gmail.com>
2020-06-16 18:42:10 -0400

    create search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { InputGroup, InputGroupText, Input, Card, CardText, CardBody,
-  CardTitle } from 'reactstrap';
+  CardTitle, Button } from 'reactstrap';
 import * as Cards from '../styles/Cards';
 import { queryNutritionInfo } from "../components/USDA-API/API-caller";
 
 const Search = () => {
   const [items, setItems] = React.useState([]);
+  const [query, setQuery] = React.useState('');
 
   React.useEffect(() => {
     queryNutritionInfo('burger')
@@ -31,13 +32,21 @@ const Search = () => {
       </div>
   ));
 
+  const queryFoods = () => {
+    queryNutritionInfo(query)
+    .then((items) => {
+      setItems(items.foods);
+    })
+  };
+
   return (
     <div className="container">
       <div className="row">
         <InputGroup>
           <InputGroupText>
-            <Input placeholder="Search Here..."/>
+            <Input value={query} onInput={e => setQuery(e.target.value)} placeholder="Search Here..."/>
           </InputGroupText>
+          <Button outline color="primary" onClick={queryFoods}>Find Foods</Button>{' '}
         </InputGroup>
       </div>
       <div className="row">{usdaItems}</div>
